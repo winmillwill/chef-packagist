2a4f2ad97b1c5b33754338be44e156f6ccaa4000
Will Milton <wa.milton@gmail.com>
2014-06-04 08:27:31 -0500

    fixture for db_master role

--- test/fixtures/roles/db_master.rb
+++ test/fixtures/roles/db_master.rb
@@ -1,3 +1,8 @@
 name 'db_master'
 description 'master of all dbs'
-run_list 'recipe[database::mysql]', 'recipe[database::master]'
+run_list 'recipe[apt]', 'recipe[database::mysql]', 'recipe[database::master]'
+default_attributes 'mysql' => {
+  'server_root_password' => 'pass',
+  'server_repl_password' => 'pass',
+  'server_debian_password' => 'pass'
+}
--- test/integration/cookbooks/test_packagist/recipes/default.rb
+++ test/integration/cookbooks/test_packagist/recipes/default.rb
@@ -6,18 +6,8 @@
 #
 # 
 
-include_recipe 'solr_app'
-solr_app 'packagist' do
-  cookbook cookbook_name.to_s
-end
-
-remote_file File.join(node.solr_app.solr_home, 'packagist', 'conf', 'schema.xml') do
-  source 'https://raw.githubusercontent.com/composer/packagist/master/doc/schema.xml'
-  notifies :restart, 'service[tomcat]'
-  owner 'tomcat6'
-  group 'tomcat6'
-end
-
+include_recipe 'apt' if platform_family? 'debian'
+include_recipe 'application_solr'
 hostsfile_entry '127.0.1.1' do
   hostname 'packagist.dev'
   action :append
