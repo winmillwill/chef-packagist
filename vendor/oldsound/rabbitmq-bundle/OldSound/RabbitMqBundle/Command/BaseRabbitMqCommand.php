4df591ea96c240dac60dacb91d66f419fec7a98a
Will Milton <wa.milton@gmail.com>
2014-11-14 09:15:19 -0600

    clearing away things that should not have been versioned

--- vendor/oldsound/rabbitmq-bundle/OldSound/RabbitMqBundle/Command/BaseRabbitMqCommand.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace OldSound\RabbitMqBundle\Command;
-
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand as Command;
-
-abstract class BaseRabbitMqCommand extends Command
-{
-}
